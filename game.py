fa95e2761bf0b6f1f3ecb7a97f9cfded30896e15
farwa-ahmed <Farwa.Ahmed@laetottenham.ac.uk>
2024-12-30 00:05:45 +0000

    room 1 completed

--- game.py
+++ game.py
@@ -54,14 +54,77 @@ mysteriousbook=object("mysterious book")
 
 
 #--------------------------------------------------------------------------------
-styled_text=pyfiglet.figlet_format('welcome to the treasure hunt game',font='doom')
+def openmap():
+    print("map")
+
+def talktonpc1():
+    print("")
+    print("welcome to the maze of secrets")
+    print("your task is to get through all the rooms and collect the magical stone to escape")
+    print("be careful, there are monsters that can kill you, if you die you die in real life "+"\U0001F608")
+    print("dont worry, you can find weapons ti help you defeat the monsters")
+
+#-------------------------------------------------------------------------------
+styled_text=pyfiglet.figlet_format('MAZE OF SECRETS',font='doom')
 print(styled_text)
+
+backpack=[]
 
 s=" "
 while s!="s":
     s=input("enter s to start ")
 
-print("You wake up in a dark and ominous cave.Through the shadows you can make out the ouline of a folded parchment on the ground and mysterious figure standing in the distance")
+print("You wake up in a dark and ominous cave.")
+print("Through the shadows you can make out the ouline of a folded parchment on the ground and mysterious figure standing in the distance")
+
+print(" ")
+print("press a to talk to the mysterious figure")
+print("press b to pick up the parchment")
+input1=" "
+while input1!="a" and input1!="b":
+    input1=input()
+
+hasmap=False
+completed=False
+
+
+if input1=="a":
+    output=talktonpc1()  
+    print(output)
+    completed=True
+else:
+    print(" ")
+    print("you have found the map")
+    print("the map is in your backpack, remember you can only hold 6 items at once")
+    backpack.append("map")
+    hasmap=True
+
+input2=" "
+while input2!="c":
+    print(" ")
+    print("press a to talk to the mysterious figure")
+    print("press b to pick up the parchment ")
+    if completed==True:
+        print("press c to enter the next room")
+    if hasmap==True:
+        print("press m to open map")    
+    input2=input()    
+
+    if input2=="a":
+        output=talktonpc1()  
+        print(output)
+        completed=True
+    elif input2=="b":
+        print(" ")
+        print("you have found the map")
+        print("the map is in your backpack, remember you can only hold 6 items at once")
+        backpack.append("map")
+        hasmap=True
+    elif input2=="m" and hasmap==True:
+        print(openmap())
+    elif input2=="c":
+        room2.enterroom()        
+
 
     
 
